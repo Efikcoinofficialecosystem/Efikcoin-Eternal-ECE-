abd4d6f4469ce3cf51656b30446e7897ebf5c890
Efikcoin official ecosyste <Efikcoinofficialecosystem@gmail.com>
2026-03-14 17:05:12 +0100

    Create app.js

---
+++ .github/workflows/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAEfS15Pa_xc1QaXLnKK_gPPPPCJeY-Q5s",
+  authDomain: "gen-lang-client-0209534368.firebaseapp.com",
+  projectId: "gen-lang-client-0209534368",
+  storageBucket: "gen-lang-client-0209534368.firebasestorage.app",
+  messagingSenderId: "950923881731",
+  appId: "1:950923881731:web:bfa142a5de3092404b4d36"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
